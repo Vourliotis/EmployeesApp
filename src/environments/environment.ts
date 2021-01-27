9d8a58aff68bd18f32eec4990bf34ab1f3cb316e
Xenofon <x.vourliotis@gmail.com>
2021-01-27 07:09:30 +0200

    protected accessToken

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   mapbox: {
     accessToken:
-      'pk.eyJ1Ijoidm91cmxpb3RpcyIsImEiOiJja2tlZTF6amIwMjVhMnhteWV4N2VucWFvIn0.XA9KgSq97t2MJ9r0CX5XbA',
+      'pk.eyJ1Ijoidm91cmxpb3RpcyIsImEiOiJja2tleXdnbjAwNWo0MndtcHRvNnRic25iIn0.7pMYlhumDFRKPdL9ZlbfVw',
   },
 };
 
